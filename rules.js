ca344bf622c4df36c3f9c11babcd87c6b0b9aeb2
SuicideHideout <andriux1233@gmail.com>
2016-09-21 20:48:39 +0300

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,3 +1,19 @@
+var PlugAPI = require('plugapi');
+
+new PlugAPI({
+    email: 'andriux1233@gmail.com',
+    password: 'test'
+}, function(err, bot) {
+    if (!err) {
+        bot.connect('testuojam'); // The part after https://plug.dj
+
+        bot.on('roomJoin', function(room) {
+            console.log("Joined " + room);
+        });
+    } else {
+        console.log('Error initializing plugAPI: ' + err);
+    }
+});
 bot.on('chat', function(data) {
     if (data.type == 'emote')
         console.log(data.from + data.message);
